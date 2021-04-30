e2e5c4025de1519ee983dc6b6171a4d6fa6bf779
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-04-30 16:46:29 -0500

    trying to map through goals array

--- bucket-list/src/Pages/Mylist.js
+++ bucket-list/src/Pages/Mylist.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import Create from '../Pages/Create'
 const Mylist = (props) => {
-
+  const [goals, setGoals] = useState([])
+  const getBucketList = (e) => {
+    e.preventDefault()
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/goal`).then((response) => {
+      console.log(response)
+    })
+  }
     return (
         <div>
-            <h1>Hello From My List</h1>
+            <button onClick={getBucketList}>Button</button>
+            <ul>
+              {props.goals.map((goals) => {
+                return (
+                  <li key={goals.id}></li>
+                )
+              })}
+            </ul>
         </div>
     )
 }
