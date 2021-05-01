5a3096597addbcaaaafa345307cc7548cff4658c
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-05-01 10:54:21 -0500

    map function for list working

--- bucket-list/src/Pages/Mylist.js
+++ bucket-list/src/Pages/Mylist.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import { useState } from 'react'
+import { useEffect } from 'react'
 import axios from 'axios'
 import Create from '../Pages/Create'
+import AllGoals from '../Components/AllGoals'
 const Mylist = (props) => {
   const [goals, setGoals] = useState([])
   const getBucketList = (e) => {
     e.preventDefault()
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/goal`).then((response) => {
-      console.log(response)
+      console.log(response.data.goals)
+      setGoals(response.data.goals)
+    
     })
   }
+  // const getBucketList = async () => {
+  //   try {
+  //     const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/goal`)
+  //     setGoals(response.data.results)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  // useEffect(() => {
+  //   getBucketList()
+  // }, [])
     return (
         <div>
+          {/* <AllGoals goals={goals} /> */}
             <button onClick={getBucketList}>Button</button>
             <ul>
-              {props.goals.map((goals) => {
+              {goals.map((goal) => {
                 return (
-                  <li key={goals.id}></li>
+                <li key={goal.id}>{goal.name}</li>
                 )
               })}
             </ul>
